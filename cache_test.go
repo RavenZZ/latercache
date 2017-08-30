8aaa5530d82b2b63eb398ee90d4db9e90b60a49d
RavenZZ <ravenzyj@outlook.com>
2017-08-30 18:10:05 +0800

    add pointer test

--- cache_test.go
+++ cache_test.go
@@ -69,3 +69,28 @@ func TestExpireNow(t *testing.T) {
 	group1.ExpireNow()
 	fmt.Println("==expired=====items count:", len(Cache("user1:module1").Values))
 }
+
+func TestPointer(t *testing.T) {
+	SetGlobalCacheExpire(time.Second * 8)
+	group1 := Cache("user1:module1")
+	//fmt.Println(group1)
+	group1.SetCacheGroupExpireCallback(func(group *CacheGroup) {
+		for _, val := range group.Values {
+			v := val
+			fmt.Println(*v.value.(*string))
+		}
+		fmt.Println("expired=====items count:", len(group.Values))
+	})
+	someid := RandStringRunes(10)
+	v1 := "some value"
+	group1.Push(someid, &v1)
+	v2 := "some value2"
+	group1.Push(someid, &v2)
+	v3 := "some value3"
+	group1.Push(someid, &v3)
+	v4 := "some value4"
+	group1.Push(someid, &v4)
+	time.Sleep(2 * time.Second)
+	group1.ExpireNow()
+	fmt.Println("==expired=====items count:", len(Cache("user1:module1").Values))
+}
